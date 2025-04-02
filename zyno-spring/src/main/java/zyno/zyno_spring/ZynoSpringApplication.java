5c82547e5a5ba966569f32a5f1f79bb2f68dfc18
zynoobb <jw9491@gmail.com>
2025-04-03 00:04:20 +0900

    first commit

---
+++ zyno-spring/src/main/java/zyno/zyno_spring/ZynoSpringApplication.java
@@ -0,0 +1,13 @@
+package zyno.zyno_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZynoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZynoSpringApplication.class, args);
+	}
+
+}
---
+++ zyno-spring/src/test/java/zyno/zyno_spring/ZynoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package zyno.zyno_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZynoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
