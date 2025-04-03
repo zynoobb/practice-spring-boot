379a6d34920db614ef18ed9f9f064041e0796b6d
zynoobb <jw9491@gmail.com>
2025-04-03 21:56:33 +0900

    Feat : section4 완료

---
+++ zyno-spring/src/main/java/zyno/zyno_spring/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package zyno.zyno_spring.repository;
+
+import zyno.zyno_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
---
+++ zyno-spring/src/main/java/zyno/zyno_spring/service/MemberService.java
@@ -0,0 +1,37 @@
+package zyno.zyno_spring.service;
+
+import zyno.zyno_spring.domain.Member;
+import zyno.zyno_spring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName((member.getName())).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
