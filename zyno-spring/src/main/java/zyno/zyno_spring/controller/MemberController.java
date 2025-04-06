113af0572291f9704d21854df47c9d8c9743a169
zynoobb <jw9491@gmail.com>
2025-04-06 20:43:39 +0900

    Feat : section6 학습 (회원 웹 기능 실습)

--- zyno-spring/src/main/java/zyno/zyno_spring/controller/MemberController.java
+++ zyno-spring/src/main/java/zyno/zyno_spring/controller/MemberController.java
@@ -2,7 +2,13 @@ package zyno.zyno_spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import zyno.zyno_spring.domain.Member;
 import zyno.zyno_spring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,27 @@ public class MemberController {
     @Autowired // DI 생성자 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        System.out.println("createForm");
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println("member = " + member.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
